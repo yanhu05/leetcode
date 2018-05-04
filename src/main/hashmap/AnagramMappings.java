2bd1031b920c6a443d61727a1c940f4b88809456
Yan Hu <wuxi.yanhu@gmail.com>
2018-05-04 00:17:07 -0400

    Code format

--- src/main/hashmap/AnagramMappings.java
+++ src/main/hashmap/AnagramMappings.java
@@ -5,19 +5,19 @@ import java.util.Map;
 
 public class AnagramMappings {
 
-    int[] anagramMappings(int[] listA, int[] listB){
+    int[] anagramMappings(int[] listA, int[] listB) {
         int[] listP = new int[0];
-        if(listA == null || listB == null){
-            return  listP;
+        if (listA == null || listB == null) {
+            return listP;
         }
         listP = new int[listA.length];
         Map<Integer, Integer> myMap = new HashMap<>();
-        for(int i= 0 ; i < listB.length; i++){
-            myMap.put(listB[i],i);
+        for (int i = 0; i < listB.length; i++) {
+            myMap.put(listB[i], i);
         }
-        for(int i= 0 ; i < listA.length; i++){
-            listP[i] =  myMap.get(listA[i]);
+        for (int i = 0; i < listA.length; i++) {
+            listP[i] = myMap.get(listA[i]);
         }
-        return  listP;
+        return listP;
     }
 }
