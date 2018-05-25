9e965f73c4d77113bde32139899c94a0467e4f07
Yan Hu <wuxi.yanhu@gmail.com>
2018-05-25 00:01:08 -0400

    Working version

--- src/main/dp/NumOfLIS.java
+++ src/main/dp/NumOfLIS.java
@@ -3,9 +3,6 @@ package main.dp;
 //https://leetcode.com/problems/number-of-longest-increasing-subsequence/description/
 
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class NumOfLIS {
     public int findNumberOfLIS(int[] nums) {
         if (nums == null || nums.length == 0) {
@@ -16,12 +13,10 @@ public class NumOfLIS {
         int[] dp = new int[len];
         dp[0] = 1;
 
-        Set<Integer> mySet = new HashSet();
-        mySet.add(nums[0]);
+
 
         int max = 1;
         for (int i = 1; i < len; i++) {
-            mySet.add(nums[i]);
             int maxHere = 1;
             for (int j = 0; j < i; j++) {
                 if (nums[j] < nums[i]) {
@@ -30,26 +25,31 @@ public class NumOfLIS {
             }
             dp[i] = maxHere;
             max = Math.max(max, maxHere);
-
-        }
-
-        if (mySet.size() == 1) {
-            return nums.length;
         }
 
 
-        int count = 0;
-        for (int i = 1; i < dp.length; i++) {
-            if (dp[i] < max) {
-                continue;
-            }
-            for (int j = 0; j < i; j++) {
-                if (dp[j] == (max - 1) && nums[j] < nums[i]) {
-                    count++;
+        int[] cnt = new int[len];
+        cnt[0] = 1;
+        for (int i = 1; i < len; i++) {
+            for (int j = 0 ; j < i; j++){
+                if (nums[j] < nums[i]){
+                    if(dp[j]+1 ==  dp[i]){
+                        cnt[i]+=cnt[j];
+                    }
                 }
             }
-
+            if(cnt[i] == 0){
+                cnt[i] = 1;
+            }
         }
+
+        int count = 0;
+        for (int i = 0; i < len; i++) {
+           if(max == dp[i]){
+               count += cnt[i];
+           }
+        }
+
         return count;
     }
 }
