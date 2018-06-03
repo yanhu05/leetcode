e1073776f5f449abace34367f8c3180a58bd0b6d
Yan Hu <wuxi.yanhu@gmail.com>
2018-06-03 00:34:45 -0400

    Cut Rod Brute Force, Memoization, Tabulation

--- src/main/dp/CutRod.java
+++ src/main/dp/CutRod.java
@@ -1,18 +1,62 @@
 package main.dp;
 
+
+import java.util.Arrays;
+
 public class CutRod {
 
-    public int cutRod(int len, int[] prices) {
+    public int cutRodBF(int len, int[] prices) {
         if (len == 0 || prices == null) return 0;
         int max = 0;
-        for (int i = 0; i < prices.length; i++) {
-            int subLen = i + 1;
-            if (subLen <= len) {
-                int localMax = prices[i];
-                localMax += cutRod(len - subLen, prices);
-                max = Math.max(max, localMax);
-            }
+
+        for (int i = 0; i < len; i++) {
+            max = Math.max(max, prices[i] + cutRodBF(len - i - 1, prices));
         }
         return max;
     }
+
+
+    public int cutRodMemoization(int len, int[] prices) {
+        if (len == 0 || prices == null) return 0;
+        int[] mem = new int[len];
+        Arrays.fill(mem, -1);
+
+        return cutRod(mem, len, prices);
+    }
+
+
+    private int cutRod(int[] mem, int len, int[] prices) {
+        if (len == 0) return 0;
+        if (mem[len - 1] > -1) {
+            return mem[len - 1];
+        }
+
+        int max = 0;
+
+        for (int i = 0; i < len; i++) {
+            max = Math.max(max, prices[i] + cutRod(mem, len - i - 1, prices));
+        }
+        mem[len - 1] = max;
+        return max;
+    }
+
+
+    public int cutRodTabulation(int len, int[] prices) {
+        if (len == 0 || prices == null) return 0;
+        int[] dp = new int[len + 1];
+        for (int i = 0; i < prices.length; i++) {
+            dp[i + 1] = prices[i];
+        }
+
+        for (int i = 2; i <= len; i++) {
+            int max = 0;
+            for (int j = 0; j < i; j++) {
+                max = Math.max(dp[j] + dp[i - j], max);
+            }
+            dp[i] = max;
+        }
+        return dp[len];
+    }
+
+
 }
--- src/test/main/dp/CutRodTest.java
+++ src/test/main/dp/CutRodTest.java
@@ -11,6 +11,15 @@ class CutRodTest {
         CutRod cr = new CutRod();
         int rodLength = 8;
         int[] rodPrices = {1, 5, 8, 9, 10, 17, 17, 20};
-        assertEquals(22, cr.cutRod(rodLength, rodPrices));
+        assertEquals(22, cr.cutRodBF(rodLength, rodPrices));
+        assertEquals(22, cr.cutRodMemoization(rodLength, rodPrices));
+        assertEquals(22, cr.cutRodTabulation(rodLength, rodPrices));
+
+        int[] rodPrices1 = {3, 5, 8, 9, 10, 17, 17, 20};
+        assertEquals(24, cr.cutRodMemoization(rodLength, rodPrices1));
+        assertEquals(24, cr.cutRodTabulation(rodLength, rodPrices1));
+
+
+
     }
 }
