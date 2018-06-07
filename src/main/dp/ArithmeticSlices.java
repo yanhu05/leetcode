d55b241d998e354c34b00510a537b6f07ec88fb8
Yan Hu <wuxi.yanhu@gmail.com>
2018-06-06 20:44:58 -0400

    Arithmetic Slices

--- src/main/dp/ArithmeticSlices.java
+++ src/main/dp/ArithmeticSlices.java
@@ -3,7 +3,25 @@ package main.dp;
 //https://leetcode.com/problems/arithmetic-slices/description/
 
 public class ArithmeticSlices {
+
     public int numberOfArithmeticSlices(int[] input) {
-        return  0;
+        if (input == null || input.length < 3) {
+            return 0;
+        }
+        int[] dp = new int[input.length];
+        for (int i = 2; i < input.length; i++) {
+            if (isConsecutive(input, i)) {
+                dp[i] = dp[i - 1] + 1;
+            }
+        }
+        int total = 0;
+        for (int i = 0; i < dp.length; i++) {
+            total += dp[i];
+        }
+        return total;
+    }
+
+    private boolean isConsecutive(int[] input, int start) {
+        return input[start] - input[start - 1] == input[start - 1] - input[start - 2];
     }
 }
