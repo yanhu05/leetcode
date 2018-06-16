c4bd4b366243fbefcb51baf2893b8e20efa5217a
Yan Hu <wuxi.yanhu@gmail.com>
2018-06-16 19:45:35 -0400

    Self Dividing Numbers

---
+++ src/main/Math/SelfDividingNumbers.java
@@ -0,0 +1,26 @@
+package main.Math;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SelfDividingNumbers {
+
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = left; i <= right; i++) {
+            char[] chars = String.valueOf(i).toCharArray();
+            boolean selfDividing = true;
+            for (int j = 0; j < chars.length; j++) {
+                int num = chars[j] - '0';
+                if (num == 0 || i % num != 0) {
+                    selfDividing = false;
+                    break;
+                }
+            }
+            if (selfDividing) {
+                list.add(i);
+            }
+        }
+        return list;
+    }
+}
---
+++ src/test/main/Math/SelfDividingNumbersTest.java
@@ -0,0 +1,13 @@
+package main.Math;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SelfDividingNumbersTest {
+
+    @Test
+    void selfDividingNumbers() {
+        assertEquals(13, new SelfDividingNumbers().selfDividingNumbers(1, 22).size());
+    }
+}
