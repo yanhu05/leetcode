1ac1def383d67da6c538786ece4107db079d57d3
Yan Hu <wuxi.yanhu@gmail.com>
2018-06-23 12:40:14 -0400

    Partition Labels

---
+++ src/main/greedy/PartitionLabels.java
@@ -0,0 +1,54 @@
+package main.greedy;
+
+/*
+
+https://leetcode.com/problems/partition-labels/description/
+
+Must work out basic algo before coding
+
+1. Basic algo is a) Find index of the last occurrence of the current char and append that part to the current one.
+   And then the new one might need to expend again due to newly added chars. At this point, we know the baisc algo
+2: Now we improve it. How to find the index of th last occurrence efficiently.  Brute force is using another loop. But that would be O(n^2) solution
+
+*/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PartitionLabels {
+
+    public List<Integer> partitionLabels(String S) {
+        List<Integer> list = new ArrayList<>();
+        int[] visited = new int[26];
+        int[] lastPOS = new int[26];
+
+        for (int i = 0; i < S.length(); i++) {
+            char c = S.charAt(i);
+            lastPOS[c - 'a'] = i;
+        }
+        int secondPointer = 0;
+        int start = 0;
+        for (int i = 0; i < S.length(); i++) {
+            char c = S.charAt(i);
+            int index = c - 'a';
+            if (visited[index] > 0) {
+                if (secondPointer == i) {
+                    list.add(i - start + 1);
+                    start = i + 1;
+                }
+                continue;
+            }
+            visited[index] = c;
+            int lastPos = lastPOS[index];
+            if (lastPos > secondPointer) {
+                secondPointer = lastPos;
+            }
+
+            if (secondPointer == i) {
+                list.add(i - start + 1);
+                start = i + 1;
+            }
+        }
+        return list;
+    }
+}
