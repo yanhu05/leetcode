83d43d39c48d919be0a553e6bd6b4f5fc1ba653c
Yan Hu <wuxi.yanhu@gmail.com>
2018-06-24 17:53:22 -0400

    Couples Holding Hands

---
+++ src/main/greedy/CouplesHoldingHands.java
@@ -0,0 +1,37 @@
+package main.greedy;
+/*
+
+https://leetcode.com/problems/couples-holding-hands/description/
+
+misguided by the difficulty level. Tried to solve it using greedy or union find
+
+Lesson learned:  Always try brute force first
+
+*/
+
+public class CouplesHoldingHands {
+
+    public int minSwapsCouples(int[] row) {
+        int cnt = 0;
+        for (int i = 1; i < row.length; i = i + 2) {
+            if (Math.abs(row[i] - row[i - 1]) == 1) {
+                if ((row[i] > row[i - 1] && row[i] % 2 > 0) ||
+                        (row[i] < row[i - 1] && row[i - 1] % 2 > 0)) {
+                    continue;
+                }
+            }
+
+            // found a non couple
+            int target = row[i - 1] % 2 == 0 ? row[i - 1] + 1 : row[i - 1] - 1;
+            for (int j = i + 1; j < row.length; j++) {
+                if (row[j] == target) {
+                    row[j] = row[i];
+                    row[i] = target;
+                    cnt++;
+                    break;
+                }
+            }
+        }
+        return cnt;
+    }
+}
