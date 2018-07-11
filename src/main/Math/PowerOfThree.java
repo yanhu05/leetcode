6ec396913ce1ad778a3206b6ff0b04a54be43f61
Yan Hu <wuxi.yanhu@gmail.com>
2018-07-10 22:19:52 -0400

    Power of three

---
+++ src/main/Math/PowerOfThree.java
@@ -0,0 +1,13 @@
+package main.Math;
+
+public class PowerOfThree {
+
+    public boolean isPowerOfThree(int n) {
+        if (n < 3) return false;
+        while (n >= 3) {
+            if (n % 3 != 0) return false;
+            n = n / 3;
+        }
+        return n == 1;
+    }
+}
