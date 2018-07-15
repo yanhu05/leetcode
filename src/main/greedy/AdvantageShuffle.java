6ca97f05bcbd52eb3ab8c92faaef1a2d2cef6c7b
Yan Hu <wuxi.yanhu@gmail.com>
2018-07-15 16:56:33 -0400

    Advantage Shuffle  refactoring

--- src/main/greedy/AdvantageShuffle.java
+++ src/main/greedy/AdvantageShuffle.java
@@ -12,41 +12,23 @@ import java.util.Arrays;
 public class AdvantageShuffle {
 
     public int[] advantageCount(int[] A, int[] B) {
-        int[] _b = B.clone();
-        Arrays.sort(B);
         Arrays.sort(A);
-        int[] tmp = new int[A.length];
+        int[] res = new int[A.length];
         boolean[] used = new boolean[A.length];
-        Arrays.fill(tmp, -1);
-        int k = 0;
         for (int i = 0; i < B.length; i++) {
-            for (int j = k; j < A.length; j++) {
-                if (A[j] <= B[i]) continue;
-                tmp[i] = A[j];
-                used[j] = true;
-                k = j + 1;
-                break;
-            }
-        }
-
-        for (int i = 0; i < tmp.length; i++) {
-            if (tmp[i] != -1) continue;
+            boolean found = false;
             for (int j = 0; j < A.length; j++) {
-                if (used[j]) continue;
+                if (A[j] <= B[i] || used[j]) continue;
+                res[i] = A[j];
                 used[j] = true;
-                tmp[i] = A[j];
+                found = true;
                 break;
             }
-        }
-        Arrays.fill(used, false);
-
-        // restore
-        int[] res = new int[B.length];
-        for (int i = 0; i < _b.length; i++) {
-            for (int j = 0; j < B.length; j++) {
-                if (_b[i] == B[j] && !used[j]) {
+            if (!found) {
+                for (int j = 0; j < A.length; j++) {
+                    if (used[j]) continue;
+                    res[i] = A[j];
                     used[j] = true;
-                    res[i] = tmp[j];
                     break;
                 }
             }
