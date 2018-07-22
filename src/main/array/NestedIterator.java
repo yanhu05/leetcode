fa9cb6d801097a95640b38d9f28d7d7ddbf05c19
Yan Hu <wuxi.yanhu@gmail.com>
2018-07-22 13:12:08 -0400

    Nested Iterator

---
+++ src/main/array/NestedIterator.java
@@ -0,0 +1,49 @@
+package main.array;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class NestedIterator implements Iterator<Integer> {
+
+    Queue<Integer> q = new LinkedList<>();
+
+    public NestedIterator(List<NestedInteger> nestedList) {
+        for (NestedInteger i : nestedList) {
+            if (i.isInteger()) {
+                q.offer(i.getInteger());
+                continue;
+            }
+            flatten(i);
+        }
+    }
+
+    private void flatten(NestedInteger ni) {
+        if (ni.isInteger()) {
+            q.offer(ni.getInteger());
+            return;
+        }
+        List<NestedInteger> nestedList = ni.getList();
+        for (NestedInteger i : nestedList) {
+            if (i.isInteger()) {
+                q.offer(i.getInteger());
+                continue;
+            }
+            flatten(i);
+        }
+    }
+
+
+    @Override
+    public Integer next() {
+        return q.isEmpty() ? null : q.poll();
+
+    }
+
+    @Override
+    public boolean hasNext() {
+        return !q.isEmpty();
+    }
+
+}
