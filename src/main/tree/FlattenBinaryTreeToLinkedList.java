12ff12e0f599488d71abab274835b65ba3061430
Yan Hu <wuxi.yanhu@gmail.com>
2018-11-22 22:20:38 -0500

    Flatten tree to linked List

--- src/main/tree/FlattenBinaryTreeToLinkedList.java
+++ src/main/tree/FlattenBinaryTreeToLinkedList.java
@@ -1,11 +1,24 @@
 package main.tree;
+/*
+
+https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+
+Thought process
+
+Need to detect that very subtree is a pre oder traversal
+
+To solve a tree problem in general
+   Does level, pre, in or post order traversal work?
+   pretend subtree has resolve issue
+
+*/
 
 public class FlattenBinaryTreeToLinkedList {
 
     public void flatten(TreeNode root) {
         if (root == null) return;
-        flatten(root.left);
-        flatten(root.right);
+        flatten(root.left);  // assume it has done pre order traversal
+        flatten(root.right); //  assume it has done pre order traversal
         TreeNode tmp = root.left;
         while (tmp != null && tmp.right != null) {
             tmp = tmp.right;
